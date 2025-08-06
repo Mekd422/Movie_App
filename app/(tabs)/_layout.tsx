7021195aebe8d87d9200cf33b16c132dc75c1d51
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-06 20:46:56 +0300

    final touch on bottom nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,7 +17,7 @@ const TabIcon: React.FC<TabIconProps> = ({ focused, icon, title }: any) => {
             <ImageBackground
             source={images.highlight}
             className='flex flex-row w-full flex-1 
-            min-w-[112px] min-h-14 mt-4 justify-center items-center
+            min-w-[112px] min-h-16 mt-4 justify-center items-center
             rounded-full overflow-hidden'>
                 <Image source={icon} tintColor='#151312'
                 className='size-5'/>
@@ -36,7 +36,27 @@ const TabIcon: React.FC<TabIconProps> = ({ focused, icon, title }: any) => {
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs 
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height:'100%',
+                justifyContent:'center',
+                alignItems:'center'
+            },
+            tabBarStyle: {
+                backgroundColor: '#0f0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '0f0d23'
+            }
+        }} >
         <Tabs.Screen
         name="index"
         options={{
