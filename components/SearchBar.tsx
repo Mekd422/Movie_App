c78fb626ef90183dac3e26f14a30b2169a1d42a9
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-07 13:35:39 +0300

    search bar component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,15 +2,20 @@ import { View, Text, Image, TextInput } from 'react-native'
 import React from 'react'
 import { icons } from '@/constants/icons'
 
-const SearchBar = () => {
+interface props{
+    placeholder: string;
+    onpress?: () => void
+}
+
+const SearchBar = ({placeholder, onpress}: props) => {
   return (
     <View className='flex-row items-center bg-dark-200
     rounded-full px-5 py-4'>
       <Image source={icons.search} className='size-5
       ' resizeMode='contain' tintColor="#ab8bff"/>
       <TextInput
-      onPress={() =>{}}
-      placeholder='search'
+      onPress={onpress}
+      placeholder={placeholder}
       value=''
       onChangeText={()=>{}}
       placeholderTextColor="#a8b5db"
