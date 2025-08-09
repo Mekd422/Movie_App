87e4ea9f941e0716ff020b6e11bb34e18c6def12
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-09 14:49:30 +0300

    search tab

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
 
 const search = () => {
+  const router = useRouter();
+
+  const {
+    data: movies, 
+    loading: moviesLoading, 
+    error: moviesError} = useFetch(() => fetchMovies({
+      query: ''
+    }))
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className='flex-1 absolute w-full z-0 
+      ' resizeMode='cover'/>
+      <FlatList 
+      data={movies}
+      renderItem={({item}) => <MovieCard {... item}}/>
+      
     </View>
   )
 }
