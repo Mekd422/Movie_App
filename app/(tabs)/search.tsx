7680540b81674b5ce4e8dbba07ef2999b8bf3c91
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-09 15:07:11 +0300

    loading state

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,18 +1,20 @@
-import { View, Text, Image, FlatList } from 'react-native'
+import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router'
 import useFetch from '@/services/useFetch'
 import { fetchMovies } from '@/services/api'
 import { images } from '@/constants/images'
 import MovieCard from '@/components/MovieCard'
+import { icons } from '@/constants/icons'
+import SearchBar from '@/components/SearchBar'
 
 const search = () => {
   const router = useRouter();
 
   const {
     data: movies, 
-    loading: moviesLoading, 
-    error: moviesError} = useFetch(() => fetchMovies({
+    loading: Loading, 
+    error: Error} = useFetch(() => fetchMovies({
       query: ''
     }))
   return (
@@ -21,7 +23,33 @@ const search = () => {
       ' resizeMode='cover'/>
       <FlatList 
       data={movies}
-      renderItem={({item}) => <MovieCard {... item}}/>
+      renderItem={({item}) => <MovieCard {... item}/>}
+      keyExtractor={(item) => item.id.toString()} 
+      
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent: "center",
+              gap: 16,
+              marginVertical: 16
+            }}
+            className="px-5"
+            contentContainerStyle={{paddingBottom: 100}}
+            ListHeaderComponent={
+              <>
+              <View className='w-full flex-row justify-center mt-20
+              items-center'>
+                <Image source={icons.logo} className='w-12 h-10'/>
+              </View>
+              <View className='my-5'>
+                <SearchBar placeholder='search movies...'/>
+              </View>
+              {Loading && (
+                <ActivityIndicator size="large" color="#0000ff"
+                className='my-3'/>
+              )}
+              </>
+            }
+            />
       
     </View>
   )
