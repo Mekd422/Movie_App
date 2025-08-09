9504bdca3b65b5370890b6ecca0415249f9e0b6a
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-09 16:21:59 +0300

    empty component reactnative prop

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -40,47 +40,56 @@ const Search = () => {
       data={movies}
       renderItem={({item}) => <MovieCard {... item} />}
       keyExtractor={(item) => item.id.toString()} 
+      className='px-5'
+      numColumns={3}
+      columnWrapperStyle={{
+        justifyContent: "center",
+        gap: 16,
+        marginVertical: 16
+      }}
       
-            numColumns={3}
-            columnWrapperStyle={{
-              justifyContent: "center",
-              gap: 16,
-              marginVertical: 16
-            }}
-            
-            contentContainerStyle={{paddingBottom: 100}}
-            ListHeaderComponent={
-              <>
-              <View className='w-full flex-row justify-center mt-20
-              items-center'>
-                <Image source={icons.logo} className='w-12 h-10'/>
-              </View>
-              <View className='my-5'>
-                <SearchBar 
-                onChangeText={(text: string) => setSearchQuery(text)}
-                value={searchQuery}
-                placeholder='search movies...'/>
-              </View>
-              {Loading && (
-                <ActivityIndicator size="large" color="#0000ff"
-                className='my-3'/>
-              )}
+      contentContainerStyle={{paddingBottom: 100}}
+      ListHeaderComponent={
+        <>
+        <View className='w-full flex-row justify-center mt-20
+        items-center'>
+          <Image source={icons.logo} className='w-12 h-10'/>
+        </View>
+        <View className='my-5'>
+          <SearchBar 
+          onChangeText={(text: string) => setSearchQuery(text)}
+          value={searchQuery}
+          placeholder='search movies...'/>
+        </View>
+        {Loading && (
+          <ActivityIndicator size="large" color="#0000ff"
+          className='my-3'/>
+        )}
 
-              {Error && (
-                <Text className='text-red-500 px-5 my-3'>
-                  Error: {Error.message}
-                </Text>
-              )}
-              {!Loading && !Error && searchQuery.trim() && Array.isArray(movies) && movies?.length > 0 &&
-              (
-                <Text className='text-xl text-white font-bold'>
-                  Search Results for{' '}
-                  <Text className='text-accent'>{searchQuery}</Text>
-                </Text>
-              )}
-              </>
-            }
-            />
+        {Error && (
+          <Text className='text-red-500 px-5 my-3'>
+            Error: {Error.message}
+          </Text>
+        )}
+        {!Loading && !Error && searchQuery.trim() && Array.isArray(movies) && movies?.length > 0 &&
+        (
+          <Text className='text-xl text-white font-bold'>
+            Search Results for{' '}
+            <Text className='text-accent'>{searchQuery}</Text>
+          </Text>
+        )}
+        </>
+      }
+      ListEmptyComponent={
+        !Loading && !Error ? (
+          <View className='mt-10 px-5'>
+            <Text className='text-center text-gray-500'>
+              {searchQuery.trim() ? "No Movies found" : "search for a movie"}
+            </Text>
+          </View>
+        ): null
+      }
+      />
       
     </View>
   )
